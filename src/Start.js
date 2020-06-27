458769a6f69f8d6c9b6bd49d614cb732e3d55319
cchalopin <clement.chalopin@gmail.com>
2020-06-28 00:18:23 +0200

    add: recode audio

--- src/Start.js
+++ src/Start.js
@@ -1,22 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Start.css';
 import { create_list_calc } from './utils';
 import Time from './Time'
 
+const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
+
+const recorde_audio = (option) => {
+    return new Promise((res, rej) => {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+                const mediaRecorder = new MediaRecorder(stream)
+
+                mediaRecorder.start()
+
+                const audioChunks = []
+
+                mediaRecorder.addEventListener("dataavailable", event => {
+                    audioChunks.push(event.data)
+                })
+
+                mediaRecorder.addEventListener("stop", () => {
+                    const audioBlob = new Blob(audioChunks)
+                    const audioUrl = URL.createObjectURL(audioBlob)
+                    const audio = new Audio(audioUrl)
+                    res(audio)
+                })
+
+                setTimeout(() => {
+                    mediaRecorder.stop()
+                }, (option.time * option.size) * 1000)
+            })
+    })
+}
 
 const Start = (props) => {
     const calc_list = create_list_calc(props.option)
-
     const [index, setIndex] = useState(0)
 
-    let inteval = setInterval(() => {
+    useInterval(() => { 
         setIndex(index + 1)
-    }, props.option.time * 1000)
-        
-        
+    }, props.option.time * 1000);
+
+    recorde_audio(props.option).then(audio => console.log(audio))
+
     return (
         <div className="start">
-            <Time {...props}/>
+            <Time {...props} />
             <div className="chrono"><h3>{props.option.size} operations</h3></div>
             <div className="opp">
                 <h1>{calc_list[index]}</h1>
